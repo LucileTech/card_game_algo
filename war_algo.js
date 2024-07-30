d3c16a0be33486fc4e450754c8977fe2d95b3a94
LucileTech <lucile.t.tech@gmail.com>
2024-07-30 10:02:23 +0200

    js implementation

--- war_algo.js
+++ war_algo.js
@@ -1,152 +1,99 @@
+//Test Case 1:
+
+let playerOneCards1 = [5, 1, 13, 10, 11, 3, 2, 10, 4, 12, 5, 11, 10, 5, 7, 6, 6, 11, 9, 6, 3, 13, 6, 1, 8, 1] 
+let playerTwoCards1 = [9, 12, 8, 3, 11, 10, 1, 4, 2, 4, 7, 9, 13, 8, 2, 13, 7, 4, 2, 8, 9, 12, 3, 12, 7, 5]
+
+// Challenge outputs
+// Test Case 1: 1
+
+//Test Case 2:
+
+// let playerOneCards1 = [3, 11, 6, 12, 2, 13, 5, 7, 10, 3, 10, 4, 12, 11, 1, 13, 12, 2, 1, 7, 10, 6, 12, 5, 8, 1] 
+// let playerTwoCards1 = [9, 10, 7, 9, 5, 2, 6, 1, 11, 11, 7, 9, 3, 4, 8, 3, 4, 8, 8, 4, 6, 9, 13, 2, 13, 5]
+
+// Challenge outputs
+// Test Case 2: 2
+
+//Test Case 3:
+
+// let playerOneCards1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] 
+// let playerTwoCards1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+// Challenge outputs
+// Test Case 3: 0
+
+
 // The card game War
-const war = (playerOne, playerTwo, breakOut) => {
-  
-	let result
-  
-	while (result === undefined) {
-
-		// If Player 1 has no cards, set the result to 2 to exit the while loop and indicate Player 2 has won
-		if (playerOne.length === 0) {
-
-			result = 2
-
-		// If Player 2 has no cards, set the result to 1 to exit the while loop and indicate Player 1 has won	
-		} else if (playerTwo.length === 0) {
-
-			result = 1
-
-		// If Player 1's card is greater than Player 2's card
-		} else if (playerOne[0] > playerTwo[0]) {
-			
-			// Add both cards to the bottom of Player 1's deck, remove from the top of both players decks, and continue the game from the start	
-			playerOne.push(playerOne.splice(0, 1)[0], playerTwo.splice(0, 1)[0])
-
-			// If a true value has been passed into the recursive use of the function, exit the loop because Player 1 has won the war
-			if (breakOut) return 1 
-		
-		// If Player 2's card is greater than Player 1's card
-		} else if (playerOne[0] < playerTwo[0]) {
-		
-			// Add both cards to the bottom of Player 2's deck, remove from the top of both players decks, and continue the game from the start
-			playerTwo.push(playerTwo.splice(0, 1)[0], playerOne.splice(0, 1)[0])
-			
-			// If a true value has been passed into the recursive use of the function, exit the loop because Player 1 has won the war
-			if (breakOut) return 2
-		
-		// If players have equal cards, initiate a war	
-		} else if (playerOne[0] === playerTwo[0]) {
-		
-			// If both players have at least four cards remaining
-			if (playerOne.slice(1).length >= 4 && playerTwo.slice(1).length >= 4) {
-				
-				// Call the handleWar helper function for a war with at least four remaining cards
-				result = handleWar(playerOne, playerTwo, 4)
-
-			// If one player has only three cards remaining
-			} else if (playerOne.slice(1).length >= 3 && playerTwo.slice(1).length >= 3) {
-				
-				// Call the handleWar helper function for a war with at least three remaining cards
-				result = handleWar(playerOne, playerTwo, 3)
-				
-			// If one player has only two cards remaining
-			} else if (playerOne.slice(1).length >= 2 && playerTwo.slice(1).length >= 2) {
-
-				// Call the handleWar helper function for a war with at least two remaining cards
-				result = handleWar(playerOne, playerTwo, 2)
-
-			// If one player has only one card remaining
-			} else if (playerOne.slice(1).length >= 1 && playerTwo.slice(1).length >= 1) {
-				
-				// Call the handleWar helper function for a war with at least one remaining cards
-				result = handleWar(playerOne, playerTwo, 1)
-			
-			// Else, exit the loop and indicate a tie	
-			} else {
-
-				result = 0
-
-			} 
-
-		}
-
-	}
-  
-	// Return 1 for a Player 1 win, 2 for a Player 2 Win, or 0 for a tie
-	return result 
-  
+const war = (player1, player2) => {
+    let count = 0
+    while ((player2.length != 0) && (player1.length != 0) ) {
+    count ++
+        //Step 1: (this is a battle)each player reveals the top card of their deck
+        // If the cards are of equal value, it's war!
+        if (player1[0] == player2[0]) {
+            subwar(player1, player2, minLength = 4, index = 0, subWarCard = 3, numberOfCards = 3)   
+        } else {
+            battle(player1, player2)
+        }
+    }
+    // in case one player has zero cards left
+    if (player2.length == 0) {
+        return 1
+    }
+    if (player1.length == 0) {
+        return 2
+    }
+    return `Somethiing weird happened... player1 ${player1}, player2 ${player2}`
+    //call the battle result
+    
 }
 
-const handleWar = (playerOne, playerTwo, cardCount) => {
-
-	// Recursively pass the cards, beginning from the "face up" card, into a new battle sequence 
-	// Pass in `true` to indicate the recursion should end after the first battle is won
-	const winner = war(playerOne.slice(cardCount), playerTwo.slice(cardCount), true)
-
-	// Add the necessary cards to the winner's deck
-	addCardsWon(winner, cardCount, playerOne, playerTwo)
-
-	// Exit the loop when a winner is resolved from recursive battles
-	return winner
-
+const battle = (player1, player2) => {
+    if (player1[0] > player2[0]) {
+        takecards(player1, player2)
+        console.log(`player 1 wins, ${player1}`)
+    } else if (player2[0] > player1[0]) {
+        takecards(player2, player1)
+        console.log(`player 2 wins, ${player2}`)
+    } else if (player2[0] == player1[0]) {
+        subwar(player1, player2)
+        return "war"
+    }
 }
 
-// Helper function for adding cards to the bottom of the deck after a war is won
-const addCardsWon = (result, num, playerOne, playerTwo) => {
+//Both players place their next three cards face down, then a card face-up.  
+//The owner of the higher face-up card wins the war and adds all cards on the table to the bottom of their deck.
 
-	// If Player 1 wins the war, add their face down cards then face up card to the bottom of their deck
-	if (result === 1) {
-	  
-		for (let i = 1; i <= num; i++) {
-			playerOne.push(playerOne[i])
-		}
-
-		// Next add Player 2's face down cards then face up card to the bottom of Player 1's deck
-		for (let i = 1; i <= num; i++) {
-			playerOne.push(playerTwo[i])
-		}
-
-		// Then add the original equal cards that triggered the war to the bottom of Player 1's deck
-		playerOne.push(playerOne[0], playerTwo[0])
-
-		//And remove the oringal cards, face down cards, and face up cards used in the war from the top of each deck
-		playerOne.splice(0, num + 1)
-		playerTwo.splice(0, num + 1)
-	
-	// If Player 2 wins the war, follow the same process but add the cards to the bottom of Player 2's deck  
-	} else if (result === 2) {
-	  
-		for (let i = 1; i <= num; i++) {
-			playerTwo.push(playerTwo[i])
-		}
-
-		for (let i = 1; i <= num; i++) {
-			playerTwo.push(playerOne[i])
-		}
-
-	  	playerTwo.push(playerTwo[0], playerOne[0])
-		playerTwo.splice(0, num + 1)
-		playerOne.splice(0, num + 1)
-
-	}
-
-	// Return the decks in their new order
-	return [playerOne, playerTwo]
-
+const subwar = (player1, player2, minLength, index, subWarCard, numberOfCards) => {
+    // TO DO: Implement If, when a war begins...
+    //...either player does not have enough cards for the war, both players reduce the number of cards to allow the war to complete (e.g. if P2 has only three cards remaining, both players play two cards down and one card up. If P2 has only one card remaining, no cards are played face-down and each player only plays one card up).
+    //...either player has no cards remaining, the other player wins.
+    //...both players have no cards remaining, the game is a draw (this is exceptionally rare in random games).
+    console.log('subwar beggining', player1, player2)
+    if ((player1.length >= minLength) && (player2.length >= minLength)) {
+        console.log('second subwar has to happen')
+        if (player1[subWarCard] > player2[subWarCard]) {
+            player1.push(...player1.splice(index, numberOfCards))
+            player1.push(...player2.splice(index, numberOfCards))
+        } else if (player2[subWarCard] > player1[subWarCard]) {
+            player2.push(...player2.splice(index, numberOfCards))
+            player2.push(...player1.splice(index, numberOfCards))
+            console.log('subwar has ended', player1, player2)
+        } else if (player2[subWarCard] == player1[subWarCard]) {
+            subSubWar(player1, player2, minLength += 3, index += 3, subWarCard += 3, numberOfCards)
+        }
+    } else {
+        console.log('not enough cards')
+    }
+    console.log('subwar in now over', player1, player2)
+}
+//If a card is higher  : add both cards to the bottom of your deck. the winner's card is added to the bottom the winner's deck first, then the loser's card.
+const takecards = (winner, looser) => {
+    //the winner's card is added to the bottom the winner's deck first
+    winner.push(winner.shift())
+    // then the loser's card
+    winner.push(looser.shift())
+    return [winner, looser]
 }
 
-module.exports = {war, handleWar, addCardsWon};
-
-// Test Cases  
-const playerOneCards1 = [5, 1, 13, 10, 11, 3, 2, 10, 4, 12, 5, 11, 10, 5, 7, 6, 6, 11, 9, 6, 3, 13, 6, 1, 8, 1]
-const playerTwoCards1 = [9, 12, 8, 3, 11, 10, 1, 4, 2, 4, 7, 9, 13, 8, 2, 13, 7, 4, 2, 8, 9, 12, 3, 12, 7, 5]
-
-const playerOneCards2 = [3, 11, 6, 12, 2, 13, 5, 7, 10, 3, 10, 4, 12, 11, 1, 13, 12, 2, 1, 7, 10, 6, 12, 5, 8, 1]
-const playerTwoCards2 = [9, 10, 7, 9, 5, 2, 6, 1, 11, 11, 7, 9, 3, 4, 8, 3, 4, 8, 8, 4, 6, 9, 13, 2, 13, 5]
-
-const playerOneCards3 = [1, 2, 3, 4, 5,  6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-const playerTwoCards3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-
-console.log(war(playerOneCards1,  playerTwoCards1)) // expect 1
-console.log(war(playerOneCards2, playerTwoCards2)) // expect 2
-console.log(war(playerOneCards3, playerTwoCards3)) // expect 0
+console.log(war(playerOneCards1, playerTwoCards1)) 
